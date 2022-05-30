1a98fd372ca478b976c7778121d6a9a6019080af
danielwei123 <chenghaw@usc.edu>
2022-05-30 00:33:04 -0400

    add 414

---
+++ Array/414 Third Maximum Number.java
@@ -0,0 +1,27 @@
+// 414. Third Maximum Number
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        
+        return max3 == null ? max1 : max3;
+    }
+}

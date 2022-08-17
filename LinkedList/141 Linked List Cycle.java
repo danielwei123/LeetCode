3a4fac92594cd25d886bf6b2a86daafc584b0a9e
danielwei123 <31397647+danielwei123@users.noreply.github.com>
2022-08-17 10:10:31 -0400

    Add 141

---
+++ LinkedList/141 Linked List Cycle.java
@@ -0,0 +1,26 @@
+// 141. Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Two pointers, slow & fast
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
